e59e0eee883af9e993cf78713bd90644e690325f
Jeff Rosengarden <jeff@rosengarden.com>
2025-05-12 13:16:16 -0500

    Initial Commit

---
+++ HP41SerialReader/HP41SerialReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP41SerialReaderApp.swift
+//  HP41SerialReader
+//
+//  Created by Jeff Rosengarden on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HP41SerialReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
